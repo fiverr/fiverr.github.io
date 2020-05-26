0709686b37271ee45cf6520af34d7164f2f11ffd
omrilotan <omri@hamadgera.com>
2020-05-27 02:13:36 +0300

    add stars. Andd daily build

--- src/cards/index.js
+++ src/cards/index.js
@@ -3,25 +3,57 @@ import { arrayOf, exact, string } from 'prop-types';
 import { rel } from '../consts';
 import './index.scss';
 
-const Cards = ({ projects }) => (<ul className="cards">
-    {
-        projects.map(
-            ({ title, link, language, description }) =>
-                (<li className={language.toLowerCase()} key={link}>
-                    <a href={link} rel={rel}>
-                        <h3>{ title }</h3>
-                        <span>{ language }</span>
-                        <p>{ description }</p>
-                    </a>
-                </li>)
-        )
+class Cards extends React.Component {
+    componentDidMount() {
+        this.props.projects.forEach(
+            ({ reponame }) => {
+                fetch(`https://api.github.com/repos/fiverr/${reponame}/stargazers`)
+                    .then((res) => res.json())
+                    .then(({ length }) => {
+                        const { stars = {} } = this.state || {};
+                        stars[reponame] = length;
+                        this.setState({ stars });
+                    });
+            }
+        );
     }
-</ul>);
+
+    render() {
+        const {
+            props: { projects },
+            state
+        } = this;
+
+        const { stars = {} } = state || {};
+
+        return (<ul className="cards">
+            {
+                projects.map(
+                    ({ title, link, reponame, language, description }) =>
+                        (<li className={language.toLowerCase()} key={link}>
+                            <a href={link} rel={rel}>
+                                <h3>
+                                    { title }
+                                </h3>
+                                <span>{ language }</span>
+                                <p>
+                                    { description } {
+                                        stars[reponame] && <ins>⭐️{ stars[reponame] }</ins>
+                                    }
+                                </p>
+                            </a>
+                        </li>)
+                )
+            }
+        </ul>);
+    }
+}
 
 Cards.propTypes = {
     projects: arrayOf(exact({
         title: string,
         link: string,
+        reponame: string,
         language: string,
         description: string
     }))
